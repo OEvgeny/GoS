2c9b0126e96c3407b647d61665cd054e48e39dcb
Ivan <mitro20@hotmail.com>
2013-06-02 22:32:25 +0400

    Update creating_list.php

--- Sources/creating_list.php
+++ Sources/creating_list.php
@@ -1,5 +1,7 @@
 <?php 
+
 header("Access-Control-Allow-Origin: *");
+
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
@@ -7,31 +9,34 @@ header("Access-Control-Allow-Origin: *");
   <head>
     <title>Main</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
   </head>
 
   <body>
     <div data-role="page" id="list" >
+    <script type="text/javascript">
+        num = 10;
+    </script>
       <div data-role="header" data-position="fixed">
         <a href="#" data-rel="back" data-iconpos="notext" data-icon="arrow-l"></a>
         <h3>Достопримечательности</h3>
       </div><!-- /header -->
       <div data-role="content">
-        <div data-role="navbar">
+        <div data-role="navbar" style="padding-bottom: 25px;">
           <ul>
             <li>
 
 <?php
+
 /* Link required files. */
 require_once "WRITE_YOUR_OWN_PATH_TO/config.php";
 
 /* Define SIZE of number of sights of list */
 define("SIZE", 10);
 
-/* Get page number */
-$PAGE = (isset($_GET['page'])) ? $_GET['page'] : 0;
-
 /* Get section ID */
-$ID = (isset($_GET['section'])) ? $_GET['section'] : 0; // 0 - all, etc from tblSection
+// 0 - all, etc from tblSection
+$ID = (isset($_GET['section'])) ? $_GET['section'] : 0;
 
 /* Connect and Choose */
 mysql_connect(DB_HOST, DB_USER, DB_PASS) or die (mysql_error ());
@@ -46,62 +51,58 @@ if($ID != 0) {
   $sght .= " AND tblSight.intSectionId = $ID";
   echo '<a data-icon="bars" class="ui-btn-active ui-state-persist" 
                href="#sights">По типу</a></li>
-        <li><a data-icon="bars" href="creating_list.php" >Все';
+        <li><a data-icon="bars" href="list.php" >Все';
 } else {
   echo '<a data-icon="bars" href="#sights">По типу</a></li>
         <li><a data-icon="bars" class="ui-btn-active ui-state-persist"
-         href="creating_list.php" >Все';
+         href="list.php" >Все';
 }
 
-$tmp = SIZE + 1;  // To select
-$sght .= ' LIMIT '.$PAGE * SIZE.','.$tmp;
+$tmp = SIZE + 1;
+$sght .= ' LIMIT 0'.$tmp;
+
 ?>
 
             </a></li>
-    </ul>
+	  </ul>
         </div><!-- /navbar -->
-  <ul data-role="listview" style="padding-top:7%">
+	<ul id="sightlist" data-role="listview" style="padding-bottom: 20px;">
 
 <?php
-$sg = mysql_query($sght);
-mysql_close();
 
-$r_size = 0;  // counter of sights
+$sg = mysql_query($sght);
+
+// counter of sights
+$r_size = 0;
 
 /* Print result */
 while(($row = mysql_fetch_assoc($sg)) && ($r_size < SIZE)) {
   $r_size++;
-  echo '<li><a href="'.
-       'creating_page.php'.  
-       '?id='.$row['intSightId'].
-       '"><img src="res/images/thumb/'.
+  echo '<li><a href="creating_page.php'.
+       '?sight='.
+       $row['intSightId'].
+       '"><img src="http://cs.karelia.ru/~pilinovi/gos/res/images/thumb/'.
        str_pad($row['intSightId'], 4, "0", STR_PAD_LEFT).
-       '.jpg" alt="'.
+       '_0.jpg" alt="'.
        $row['txtNameSight'].
-       '"/><h2>'.
-       $row['txtNameSight'].
-       '</h2><p>'.
-       $row['txtNameSection'].
-       '</p></a></li>';
+       '"/>';  
+  echo '<h2>'.$row['txtNameSight'].'</h2>';
+  echo '<p>'.$row['txtNameSection'].'</p></a></li>';
 }
 
 ?>
+
         </ul>
-      </div><!-- /content -->
-      <div data-role="controlgroup" data-type="horizontal" data-mini="true"
-           style="width: 100%; text-align: center;">
+
 <?php
 
-/* Buttons to right 'n' left */
-if($PAGE > 0)
-  echo '<a href="creating_list.php?section='.$ID.'&page=',$PAGE-1,'" data-icon="arrow-l" data-iconpos="right"
-         data-role="button" data-theme="e">ВЛЕВО</a>';
+/* Button to load content */
 if($row)
-  echo '<a href="creating_list.php?section='.$ID.'&page=',$PAGE+1,'" data-icon="arrow-r" data-iconpos="left"
-         data-role="button" data-theme="e">ВПРАВО</a>';
+  echo '<div id="load" data-role="button" data-theme="e" onClick="loadList();">Ещё</div>';
 
 ?>
-      </div>
+
+      </div><!-- /content -->
       <div data-role="footer" data-position="fixed">
         <div data-role="navbar">
           <ul>
